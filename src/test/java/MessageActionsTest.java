d08d6c1238d217d9b21b597703049537fddef400
hamaianhnhi <tuananhhcmus@gmail.com>
2018-08-01 21:09:03 +0700

    renamed some test functions

--- src/test/java/MessageActionsTest.java
+++ src/test/java/MessageActionsTest.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 public class MessageActionsTest {
 
     @Test(expectedExceptions = IllegalArgumentException.class)
-    public void testRandomMessageToPublic_WithN_GreaterThan100_ShouldThrowException() throws IllegalArgumentException{
+    public void testGetWeightedRandomMessageToPublic_WithN_GreaterThan100_ShouldThrowException() throws IllegalArgumentException{
         ArrayList randomList = new ArrayList();
         for (int i = 0; i < 101; i++) {
             Radomizer radomizer = new Radomizer();
@@ -19,7 +19,7 @@ public class MessageActionsTest {
     }
 
     @Test
-    public void testRandomMessageToPublic_WithN_is100_ShouldThrowException() throws IllegalArgumentException{
+    public void testGetWeightedRandomMessageToPublic_WithN_is100_ShouldThrowException() throws IllegalArgumentException{
         ArrayList randomList = new ArrayList();
         for (int i = 0; i < 100; i++) {
             Radomizer radomizer = new Radomizer();
@@ -31,21 +31,21 @@ public class MessageActionsTest {
     }
 
     @Test
-    public void testRandomMessageToPublic_WithN_is1_ShouldThrowException() throws IllegalArgumentException{
+    public void testGetWeightedRandomMessageToPublic_WithN_is1_ShouldThrowException() throws IllegalArgumentException{
         int[] oneMemberArray = {8};
         MessageActions messageActions = new MessageActions();
         Assert.assertNotNull(messageActions.getWeightedRandomMessageToPublic(oneMemberArray));
     }
 
     @Test(expectedExceptions = IllegalArgumentException.class)
-    public void testRandomMessageToPublic_WithN_LessThan1_ShouldThrowException() throws IllegalArgumentException{
+    public void testGetWeightedRandomMessageToPublic_WithN_LessThan1_ShouldThrowException() throws IllegalArgumentException{
         int weightArray[] = {};
         MessageActions messageActions = new MessageActions();
         messageActions.getWeightedRandomMessageToPublic(weightArray);
     }
 
     @Test(expectedExceptions = IllegalArgumentException.class)
-    public void testRandomMessageToPublic_WeightGreaterThan10Pow9_ShouldThrowException() throws IllegalArgumentException{
+    public void testGetWeightedRandomMessageToPublic_WeightGreaterThan10Pow9_ShouldThrowException() throws IllegalArgumentException{
         int tenPow9 = (int)Math.pow(10, 9);
         int weightArray[] = {tenPow9 + 1};
         MessageActions messageActions = new MessageActions();
@@ -53,7 +53,7 @@ public class MessageActionsTest {
     }
 
     @Test
-    public void testRandomMessageToPublic_WeightIs10Pow9_ShouldBePassed() throws IllegalArgumentException{
+    public void testGetWeightedRandomMessageToPublic_WeightIs10Pow9_ShouldBePassed() throws IllegalArgumentException{
         int tenPow9 = (int)Math.pow(10, 9);
         int weightArray[] = {tenPow9};
         MessageActions messageActions = new MessageActions();
@@ -61,14 +61,14 @@ public class MessageActionsTest {
     }
 
     @Test(expectedExceptions = IllegalArgumentException.class)
-    public void testRandomMessageToPublic_WeightLessThan0_ShouldThrowException() throws IllegalArgumentException{
+    public void testGetWeightedRandomMessageToPublic_WeightLessThan0_ShouldThrowException() throws IllegalArgumentException{
         int weightArray[] = {-1, 5, 6};
         MessageActions messageActions = new MessageActions();
         messageActions.getWeightedRandomMessageToPublic(weightArray);
     }
 
     @Test()
-    public void testRandomMessageToPublic_WeightIs0_ShouldBePassed() throws IllegalArgumentException{
+    public void testGetWeightedRandomMessageToPublic_WeightIs0_ShouldBePassed() throws IllegalArgumentException{
         int weightArray[] = {0};
         MessageActions messageActions = new MessageActions();
         Assert.assertEquals(weightArray[messageActions.getWeightedRandomMessageToPublic(weightArray)], 0);
