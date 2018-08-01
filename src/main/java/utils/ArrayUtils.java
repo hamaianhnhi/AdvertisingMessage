d37200c1898264aee2c6afefd417f69b45e0d0a8
hamaianhnhi <tuananhhcmus@gmail.com>
2018-08-01 21:04:28 +0700

    added some doc comments

--- src/main/java/utils/ArrayUtils.java
+++ src/main/java/utils/ArrayUtils.java
@@ -4,6 +4,12 @@ import java.util.ArrayList;
 
 public class ArrayUtils {
 
+    /**
+     * This returns list of occurrence's index of an item from a given ArrayList
+     * @param item an object to be used to find occurrences
+     * @param arrayList list of items to be loop
+     * @return list of occurrence's index
+     */
     public ArrayList indexOfAll(Object item, ArrayList arrayList){
         ArrayList indexListToReturn = new ArrayList();
         for (int i = 0; i < arrayList.size(); i++) {
@@ -14,8 +20,14 @@ public class ArrayUtils {
         return indexListToReturn;
     }
 
-    public int findOccurrencesInArray(int numberToFind, ArrayList arrayList){
-        ArrayList indexOfOccurrences = indexOfAll(numberToFind, arrayList);
+    /**
+     * This returns number of occurrences of an object from given ArrayList
+     * @param objectToFind an object to be used to find occurrences
+     * @param arrayList list of items to be loop
+     * @return number of occurrences
+     */
+    public int findOccurrencesInArray(Object objectToFind, ArrayList arrayList){
+        ArrayList indexOfOccurrences = indexOfAll(objectToFind, arrayList);
         return indexOfOccurrences.size();
     }
 }
